f3ba51775e6659e3e0a4123698c9e4fde1f80379
Ryan Arcel Galendez <ryanargee2@gmail.com>
2024-07-19 19:05:59 +0800

    NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+		<header>
+			<nav>
+				<NuxtLink to="/">Nuxt Dojo</NuxtLink>
+				<ul>
+					<li><NuxtLink to="/">Home</NuxtLink></li>
+					<li><NuxtLink to="/about">About</NuxtLink></li>
+					<li><NuxtLink to="/products">Products</NuxtLink></li>
+				</ul>
+			</nav>
+		</header>
 		<h2>Home</h2>
 		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ut omnis perspiciatis quibusdam. Ex, exercitationem aperiam. Rerum non dicta qui nesciunt quae. Veritatis, aliquid similique? Facere repellendus neque beatae nisi.</p>
 		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur facilis quaerat commodi quos quas ea quo, optio enim ad perferendis ipsum aliquam porro cumque necessitatibus dolore numquam quae voluptatibus rem.</p>
