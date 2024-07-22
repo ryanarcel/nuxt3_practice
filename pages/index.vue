85ab974aecde643d5e75bf82c7a6f456ef2ee37c
Ryan Arcel Galendez <ryanargee2@gmail.com>
2024-07-22 09:34:36 +0800

    chore: run eslint on index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+	<div>
 		<h2>Home</h2>
-		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ut omnis perspiciatis quibusdam. Ex, exercitationem aperiam. Rerum non dicta qui nesciunt quae. Veritatis, aliquid similique? Facere repellendus neque beatae nisi.</p>
-		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur facilis quaerat commodi quos quas ea quo, optio enim ad perferendis ipsum aliquam porro cumque necessitatibus dolore numquam quae voluptatibus rem.</p>
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ut omnis perspiciatis quibusdam. Ex,
+			exercitationem aperiam. Rerum non dicta qui nesciunt quae. Veritatis, aliquid similique? Facere repellendus
+			neque beatae nisi.</p>
+		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur facilis quaerat commodi quos quas ea
+			quo, optio enim ad perferendis ipsum aliquam porro cumque necessitatibus dolore numquam quae voluptatibus
+			rem.
+    </p>
 	</div>
 </template>
 
@@ -11,11 +16,12 @@
 </script>
 
 <style scoped>
-	h2 {
-		margin-bottom: 20px;
-		font-size: 36px;
-	}
-	p {
-		margin: 20px 0;
-	}
+h2 {
+	margin-bottom: 20px;
+	font-size: 36px;
+}
+
+p {
+	margin: 20px 0;
+}
 </style>
